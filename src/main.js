0eab99219aff2a698236a28fa42daf9bbdd2e84f
Michel Moraes <michel.moraes@helloworldlondon.co.uk>
2018-03-30 18:16:13 +0200

    Importing jQuery and Bootstrap

--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+// Vendors
+window.$ = window.jQuery = require("jquery")
+import bootstrap from 'bootstrap'
+
 new Vue({
   el: '#app',
   render: h => h(App)
